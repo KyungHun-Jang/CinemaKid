83c6708ab28558f0d1d9cd37ec217d32ee4d4ebe
Byeonghun Hwang <byeonghwang.com>
2019-05-03 10:11:46 +0900

    Initial Commit

---
+++ CinemaKid/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  CinemaKid
+//
+//  Created by ktds 10 on 03/05/2019.
+//  Copyright © 2019 kt ds. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
